9ce57d08bcbbc83ba21e87e10883fb3291dd4535
nothotscott <idontwantyoutospam@gmail.com>
2020-12-31 10:43:33 -0600

    GDT documentation

--- src/kernel/cpu/gdt.asm
+++ src/kernel/cpu/gdt.asm
@@ -11,13 +11,18 @@ EXTERN	g_gdt_descriptor
 
 SECTION	.text
 
+; Please credit me if you're going to use this for reference :)
 GLOBAL	gdt_load
 gdt_load:
 	cli
 	lgdt	[g_gdt_descriptor]
 	; TODO move TSS segment and load (ltr)
-	pop		rsi					; keep the return address for later
-	mov		rdi, rsp			; we want to keep the old stack pointer
+	; This will exploit iretq's ability to set segments in long mode
+	; The order iretq will pop is: RIP, CS, RFLAGS, RSP, SS
+	; We first need to save data we wish to keep
+	pop		rsi					; keep the return address in rsi
+	mov		rdi, rsp			; keep the old stack pointer in rdi
+	; Now to push what iretq needs
 	push	QWORD 0x10			; push data segment: g_idt[2]
 	push	rdi					; push old stack pointer
 	pushfq						; push flags
