675d29591c9f4741558474c58c287564da623e91
nothotscott <idontwantyoutospam@gmail.com>
2020-12-30 22:46:38 -0600

    Consolidated GDT

--- src/kernel/cpu/gdt.asm
+++ src/kernel/cpu/gdt.asm
@@ -8,7 +8,6 @@
 
 
 EXTERN	g_gdt_descriptor
-EXTERN	print_char
 
 SECTION	.text
 
@@ -17,12 +16,11 @@ gdt_load:
 	cli
 	lgdt	[g_gdt_descriptor]
 	; TODO move TSS segment and load (ltr)
-	mov		rdi, rsp
+	pop		rsi					; keep the return address for later
+	mov		rdi, rsp			; we want to keep the old stack pointer
 	push	0x10				; push data segment: g_idt[2]
-	push	rdi					; push stack pointer
+	push	rdi					; push old stack pointer
 	pushfq						; push flags
 	push	QWORD 0x08			; push code segment: g_idt[1]
-	push	.flush				; push where RIP should go
+	push	rsi					; set rip to the return address from earlier
 	iretq						; far return in new code segment
-	.flush:
-		ret
